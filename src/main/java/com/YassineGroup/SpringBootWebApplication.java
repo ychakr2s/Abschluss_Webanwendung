4981ae33a4a4b6ece6d70dbc542a389b2d70fd56
Yassine <yassine.chakri.gus@gmail.com>
2019-05-08 18:10:27 +0200

    full Upload

---
+++ src/main/java/com/YassineGroup/SpringBootWebApplication.java
@@ -0,0 +1,37 @@
+package com.YassineGroup;
+
+import com.YassineGroup.controller.UploadController;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+//    private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+        FileUtils.deleteDirectory(new File(UploadController.UPLOADED_FOLDER));
+        new File(UploadController.UPLOADED_FOLDER).mkdir();
+    }
+
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+
+        return tomcat;
+    }
+}
